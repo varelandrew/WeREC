924cc6cbc1b9bada9ccc2bd147996d31cfc52144
brycechinn <33844159+brycechinn@users.noreply.github.com>
2022-02-20 03:53:52 -0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -7,16 +7,50 @@ function googleLogin() {
                 const user = result.user;
                 document.write('Hello ', user.displayName, user.uid);
                 console.log(user)
-                
+
                 const app = firebase.app();
 
                 const db = firebase.firestore();
+                
+                const usersRef = db.collection('users').doc(user.uid)
 
-                const users = db.collection('users').doc(user.uid).update(
-                    {
-                        logins: firebase.firestore.FieldValue.increment(1)
-                    }
-                );
+                usersRef.get()
+                    .then((docSnapshot) => {
+                        if (docSnapshot.exists) {
+                            usersRef.onSnapshot((doc) => {
+                                const users = db.collection('users').doc(user.uid).update(
+                                    {
+                                        logins: firebase.firestore.FieldValue.increment(1)
+                                    }
+                                );
+                        });
+                        } else {
+                            const users = db.collection('users').doc(user.uid).set(
+                                {
+                                    logins: 0
+                                }
+                            );
+                        }
+                    });
+
+                /*
+                if (user.uid.empty) {
+                    const users = db.collection('users').doc(user.uid).update(
+                        {
+                            logins: firebase.firestore.FieldValue.increment(1)
+                        }
+                    );
+                    console.log('flag1', user.uid.logins)
+                }
+                else {
+                    const users = db.collection('users').doc(user.uid).set(
+                        {
+                            logins: 0
+                        }
+                    );
+                    console.log('flag2', user.uid.logins)
+                }
+                */
 
             })
             .catch(console.log)
